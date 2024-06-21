f0a65ef237c1f0959966d3874e4e5e0acf7c6c50
Adrien Faure <adrien.faure@protonmail.com>
2024-06-21 23:18:04 +0200

    cg: wrap colored

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # cgvg.rs
 
- simple rewrite in Rust of [cgvg](https://github.com/uzi/cgvg) using [ripgrep](https://github.com/BurntSushi/ripgrep) as searching tool.
+simple rewrite in Rust of [cgvg](https://github.com/uzi/cgvg) using [ripgrep](https://github.com/BurntSushi/ripgrep) as searching tool.
 
--- src/cg/main.rs
+++ src/cg/main.rs
@@ -1,17 +1,18 @@
 use clap::Parser;
-use colored::{ColoredString, Colorize};
-use itertools::FoldWhile::{Continue, Done};
-use itertools::Itertools;
+use colored::Colorize;
 use log::{debug, info};
 use rgvg::common::{expand_paths, save, Index};
-use std::process::ExitCode;
 use std::*;
-use terminal_size::{terminal_size, Height, Width};
+use std::process::ExitCode;
+use terminal_size::terminal_size;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::Command;
 
 mod ripgrep_json;
 use ripgrep_json::Match;
+
+mod print_terminal;
+use print_terminal::{number_of_digits, wrap_text};
 
 /// rg find code using ripgrep
 #[derive(Parser, Debug)]
@@ -30,7 +31,7 @@ struct Args {
     rg: Vec<String>,
 }
 
-pub fn match_view(matched: &Match, idx: &usize) -> Option<String> {
+pub fn match_view(matched: &Match, idx: &usize, terminal_size: &usize) -> Option<String> {
     let result = match matched {
         Match::Begin { path } => Some((None, format!("{}", path.text.red()))),
         Match::Match {
@@ -40,16 +41,17 @@ pub fn match_view(matched: &Match, idx: &usize) -> Option<String> {
             absolute_offset: _,
             submatches,
         } => {
-            let colored_line_number = format!("{line_number}").cyan();
             let mut color_submatches = String::from("");
-            let remaining = String::from(lines.text.trim_end_matches('\n'));
             let mut cursor = 0;
+
+            let matched_text = String::from(lines.text.trim_end_matches('\n'));
+
             for submatch in submatches.iter() {
-                let begin = String::from(&remaining[cursor..submatch.start]);
+                let begin = String::from(&matched_text[cursor..submatch.start]).bright_green();
                 let submatch_str = format!(
                     "{}",
-                    remaining[submatch.start..submatch.end]
-                        .bright_green()
+                    matched_text[submatch.start..submatch.end]
+                        .yellow()
                         .bold()
                 );
 
@@ -58,113 +60,49 @@ pub fn match_view(matched: &Match, idx: &usize) -> Option<String> {
                 color_submatches = format!("{color_submatches}{begin}{submatch_str}");
             }
 
-            color_submatches = format!("{color_submatches}{}", &remaining[cursor..].to_string());
+            color_submatches = format!("{color_submatches}{}", &matched_text[cursor..].to_string().bright_green());
 
             let result = color_submatches;
 
-            Some((Some(line_number), result))
+            Some((Some(*line_number), result))
         }
         Match::End { path: _ } => Some((None, format!(""))),
         _ => None,
     };
 
-    let colored_idx = format!("{idx}").yellow();
-
     match &result {
         Some((Some(line_number), text)) => {
-            let size = terminal_size();
-            if let Some((Width(w), Height(h))) = size {
-                let mut result = "".to_string();
+            let mut result = "".to_string();
 
-                for s in wrap_text(text, w as usize).iter() {
-                    result = format!("{result}{s}\n");
+            let line_number_len = number_of_digits(&(*line_number as usize));
+            let idx_len = number_of_digits(&idx);
+
+            let prefix = format!(
+                "{}\t{}\t",
+                idx.to_string().cyan(),
+                line_number.to_string().magenta()
+            );
+            let prefix_size = line_number_len + idx_len + 16;
+
+            let padding = std::iter::repeat(" ")
+                .take(prefix_size - 1)
+                .collect::<String>();
+
+            let text_size = terminal_size - prefix_size;
+
+            for (line, s) in wrap_text(text, &text_size, &8).iter().enumerate() {
+                if line == 0 {
+                    result = format!("{prefix}{s}\n");
+                } else {
+                    result = format!("{result}{padding} {s}\n");
                 }
-
-                return Some(result);
-            } else {
-                debug!("Unable to get terminal size");
             }
 
-            return None
+            return Some(result);
         }
         Some((None, text)) => return Some(text.to_string()),
-        None => {return None}
+        None => return None,
     };
-
-}
-
-pub fn iter_colored(string: &str) -> impl Iterator<Item = String> + '_ {
-    string.chars().batching(|it| {
-        match it.next() {
-            Some(t) => {
-                if t == '\u{1b}' {
-                    let s: String = it.take_while(|c| *c != 'm').collect();
-                    // Since take_while consumes the first false, we mannually add the 'm'
-                    Some(format!("{t}{s}m"))
-                } else {
-                    Some(t.to_string())
-                }
-            }
-            None => None,
-        }
-    })
-}
-
-pub fn wrap_text<'a>(text: &'a str, max_length: usize) -> Vec<String> {
-    let mut memory: Vec<String> = vec![];
-
-    let wrapped = iter_colored(text)
-        .batching(|it| {
-            let (len, temp) = it
-                .fold_while((0, memory.clone()), |(length, mut acc), c| {
-                    let mut new_length = length;
-
-                    // if !memory.is_empty() {
-                    //     acc.extend(memory.clone().into_iter());
-                    // };
-
-                    acc.push(c.clone());
-
-                    if !c.starts_with('\u{1b}') {
-                        new_length += 1;
-                    } else {
-                        match c.as_str() {
-                            "\u{1b}[0m" => {
-                                memory.clear();
-                            }
-                            _ => {
-                                memory.push(c.clone());
-                            }
-                        }
-                    }
-
-                    if new_length == max_length {
-                        if !memory.is_empty() {
-                            memory = memory.clone();
-                            acc.push("\u{1b}[0m".to_string());
-                        }
-
-                        Done((new_length, acc))
-                    } else {
-                        Continue((new_length, acc))
-                    }
-                })
-                .into_inner();
-
-            let line: String = temp.into_iter().collect();
-
-            // If len is 0 then the string contains remaining style
-            // harder to clean than to ignore, and I think ignoring wont change the style
-            if len == 0 {
-                None
-            } else {
-                Some(line)
-            }
-        })
-        .map(|array| String::from(array))
-        .collect_vec();
-
-    wrapped
 }
 
 #[tokio::main]
@@ -172,11 +110,7 @@ async fn main() -> ExitCode {
     env_logger::init();
 
     let size = terminal_size();
-    if let Some((Width(w), Height(h))) = size {
-        debug!("Your terminal is {} cols wide and {} lines tall", w, h);
-    } else {
-        debug!("Unable to get terminal size");
-    }
+    let terminal_size = size.unwrap().0;
 
     let args = Args::parse();
     debug!("{:?}", args);
@@ -204,6 +138,7 @@ async fn main() -> ExitCode {
 
     let mut idx = 0;
     let mut file_and_line: Vec<Index> = vec![];
+    println!("terminal size= {:?}", terminal_size);
 
     while let Some(line) = reader.next_line().await.expect("Failed to read line") {
         debug!("Received line: {}", line);
@@ -224,7 +159,7 @@ async fn main() -> ExitCode {
             _ => {}
         };
 
-        match match_view(&matched, &idx) {
+        match match_view(&matched, &idx, &(terminal_size.0 as usize)) {
             Some(text) => {
                 println!("{text}")
             }
@@ -240,76 +175,4 @@ async fn main() -> ExitCode {
     save(file_and_line, &match_file, &index_file);
 
     return ExitCode::from(0);
-}
-
-#[cfg(test)]
-mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
-
-    #[test]
-    fn test_wrap_text() {
-        // Simple cases
-        let res = wrap_text("1234567890abc", 5);
-        assert_eq!(vec!["12345", "67890", "abc"], res);
-
-        let res = wrap_text("1234567890abc", 15);
-        assert_eq!(vec!["1234567890abc"], res);
-
-        // Got coloring
-        let blue = format!("aaaaabbbbbzzzzz").blue().to_string();
-
-        let res = wrap_text(&blue, 5);
-        assert_eq!(
-            vec![
-                "\u{1b}[34maaaaa\u{1b}[0m",
-                "\u{1b}[34mbbbbb\u{1b}[0m",
-                "\u{1b}[34mzzzzz\u{1b}[0m"
-            ],
-            res
-        );
-
-        let blue_bold_underline = format!("aaaaabbbbbzzzzz")
-            .blue()
-            .bold()
-            .underline()
-            .to_string();
-
-        let res = wrap_text(&blue_bold_underline, 5);
-
-        assert_eq!(
-            vec![
-                "\u{1b}[1;4;34maaaaa\u{1b}[0m",
-                "\u{1b}[1;4;34mbbbbb\u{1b}[0m",
-                "\u{1b}[1;4;34mzzzzz\u{1b}[0m"
-            ],
-            res
-        );
-
-        let begin = format!("{}", "aaaaa".to_string().blue());
-        let middle = format!("{}", "bbbbb".to_string().white());
-        let end = format!("{}", "zzzzz".to_string().red());
-
-        let blue_bold_underline = format!("{begin}{middle}{end}").underline().to_string();
-
-        let res = wrap_text(&blue_bold_underline, 5);
-        assert_eq!(
-            vec![
-                "\u{1b}[4m\u{1b}[34maaaaa\u{1b}[0m",
-                "\u{1b}[4m\u{1b}[34m\u{1b}[0m\u{1b}[4m\u{1b}[37mbbbbb\u{1b}[0m",
-                "\u{1b}[4m\u{1b}[37m\u{1b}[0m\u{1b}[4m\u{1b}[31mzzzzz\u{1b}[0m"
-            ],
-            res
-        );
-    }
-
-    #[test]
-    fn test_iter_colored() {
-        let blue_bold_underline = format!("abz").blue().bold().underline().to_string();
-
-        assert_eq!(
-            vec!["\u{1b}[1;4;34m", "a", "b", "z", "\u{1b}[0m"],
-            iter_colored(&blue_bold_underline).collect::<Vec<String>>()
-        );
-    }
 }
